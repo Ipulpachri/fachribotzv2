28d036c741daba52cbc1b669041e525906174cc8
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-04-15 16:50:10 +0700

    sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,8 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
 𝙎𝘾 𝙊𝙍𝙄 𝘽𝙔 𝙇𝙔𝙉𝙓𝙕𝙔 🔥
-github.com/LynnXzy/scbotz-v1
-`.trim(), '©LynXzy🥀', 'Makasih', 'Thanks')
+Recode By Fachri
+
+- 
+*Private Bg*
+`.trim(), '© ᴹᴿ᭄ Fachri botz ×፝֟͜×', 'Makasih', 'Thanks')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
