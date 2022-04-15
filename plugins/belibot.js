d815e1de2a686b8c795af277ddfa29fd17ab0e14
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-04-15 17:27:45 +0700

    belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,25 +1,25 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 SEWA BOT 〙 ═
-╠➥ *1 Minggu* : *Rm 5.00*
-╠➥ *2 Minggu* : *Rm 10.00*
-╠➥ *3 Minggu* : *Rm 15.00*
-╠➥ *1 Bulan* :  *Rm 20.00*
+╠➥ *1 Minggu* : *Rp 5.00*
+╠➥ *2 Minggu* : *Rp 10.00*
+╠➥ *3 Minggu* : *Rp 15.00*
+╠➥ *1 Bulan* :  *Rp 20.00*
 ║
 ╠═〘 USER PREMIUM 〙 ═
-╠➥ *1 Minggu* : *Rm 5.00*
-╠➥ *2 Minggu* : *Rm 10.00*
-╠➥ *3 Minggu* : *Rm 15.00*
-╠➥ *1 Bulan* :  *Rm 20.00*
+╠➥ *1 Minggu* : *Rp 5.00*
+╠➥ *2 Minggu* : *Rp 10.00*
+╠➥ *3 Minggu* : *Rp 15.00*
+╠➥ *1 Bulan* :  *Rp 20.00*
 ║
 ╠═〘 PEMBAYARAN 〙 ═
-╠➥ Topup
-╠➥ Pin
+╠➥ Gopay
+╠➥ Dana
 ║
 ╠═〘 INGIN BELI BOT? 〙 ═
-╠➥Klik Button Di Bawah
+╠➥ Klik Button Di Bawah
 ║
-╠═〘 LYNXZY 〙 ═`.trim(), '©LynnXzy', 'Digi', '#viadigi', 'Umobile', '#viaumobile', m)
+╠═〘 FachriBotz 〙 ═`.trim(), '© ᴹᴿ᭄ Fachri botz ×፝֟͜×', 'Digi', '#viadigi', 'Umobile', '#viaumobile', m)
 }
 
 handler.command = /^sewabot$/i
