926f9a9d3b2d9545feea1b1a8f20e0e1d9f2cb09
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-04-16 14:45:35 +0700

    enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -278,7 +278,7 @@ ${usedPrefix}off welcome
 let str = `
 *${type}* berhasil di *${isEnable ? 'nyala' : 'mati'}kan* ${isAll ? 'untuk bot ini' : isUser ? '' : 'untuk chat ini'}
 `.trim()
-     await conn.send2Button(m.chat, str, watermark, 'Owner', '.owner', 'Menu', '.menu', { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `© ᴹᴿ᭄ Fachri botz ×፝֟͜×`, itemCount: 999, thumbnail: fs.readFileSync('./src/93278eaa12fd253a4fe3cc08a0b219bc.jpg')}}})
+     await conn.send2Button(m.chat, str, watermark, 'Owner', '.owner', 'Menu', '.menu', { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `Fachri🌙`, itemCount: 999, thumbnail: fs.readFileSync('./src/93278eaa12fd253a4fe3cc08a0b219bc.jpg')}}})
 }
 handler.help = ['on', 'off'].map(v => v + ' <opsi>')
 handler.tags = ['group', 'owner']
