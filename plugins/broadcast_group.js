dddaf7d4a85469a11c61e7d01c74ddae817e19d4
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-04-18 06:26:15 +0700

    broadcast_grup.js

--- plugins/broadcast_group.js
+++ plugins/broadcast_group.js
@@ -17,7 +17,7 @@ const anu = {
 			"groupJid": "6285240750713-1610340626@g.us",
 			"inviteCode": "mememteeeekkeke",
 			"groupName": "Grub Bot", 
-            "caption": "「 LynnXzy Broadcast💌 」", 
+            "caption": "「  BROADCAST  」", 
             'jpegThumbnail': fs.readFileSync('./src/IMG-20220324-WA1327.jpg')
 		}
 	}
